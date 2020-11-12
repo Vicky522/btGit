51bfe89f744770f835f08e14fd97898cf392e48a
Vicky522 <vothanhvan522@gmail.com>
2020-11-12 23:42:48 +0700

    submit-form

--- server.js
+++ server.js
@@ -2,36 +2,50 @@ var express = require("express");
 var app = express();
 
 // Phuong thuc get() phan hoi mot GET Request
-app.get("/", function (req, res) {
-  console.log("GET Request");
-  res.send("Hello GET");
-});
+// app.get("/", function (req, res) {
+//   console.log("GET Request");
+//   res.send("Hello GET");
+// });
 
-app.get("/about", function (req, res) {
-  console.log("about Request");
-  res.send("Hello about");
-});
+// app.get("/about", function (req, res) {
+//   console.log("about Request");
+//   res.send("Hello about");
+// });
 
-// Phuong thuc post() phan hoi mot POST Request
-app.post("/", function (req, res) {
-  console.log("POST Request");
-  res.send("Hello POST");
-});
+// // Phuong thuc post() phan hoi mot POST Request
+// app.post("/", function (req, res) {
+//   console.log("POST Request");
+//   res.send("Hello POST");
+// });
 
-// Phuong thuc delete() phan hoi mot DELETE Request.
-app.delete("/delete", function (req, res) {
-  console.log("DELETE Request");
-  res.send("Hello DELETE");
-});
+// // Phuong thuc delete() phan hoi mot DELETE Request.
+// app.delete("/delete", function (req, res) {
+//   console.log("DELETE Request");
+//   res.send("Hello DELETE");
+// });
 
-// Phuong thuc nay phan hoi mot GET Request có dạng abcd, abxcd, ab123cd, ...
-app.get("/ab*cd", function (req, res) {
-  console.log("GET request /ab*cd");
-  res.send("Page Pattern Match");
-});
+// // Phuong thuc nay phan hoi mot GET Request có dạng abcd, abxcd, ab123cd, ...
+// app.get("/ab*cd", function (req, res) {
+//   console.log("GET request /ab*cd");
+//   res.send("Page Pattern Match");
+// });
 
 // express.static để phục vụ cho các file tĩnh như hình ảnh, css, js, ...
 app.use(express.static("public"));
+
+app.get("/index.html", function (req, res) {
+  res.sendFile(__dirname + "/" + "index.html");
+});
+
+app.get("/process_get", function (req, res) {
+  // Chuan bi output trong dinh dang JSON
+  response = {
+    first_name: req.query.first_name,
+    last_name: req.query.last_name,
+  };
+  console.log(response);
+  res.end(JSON.stringify(response));
+});
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
